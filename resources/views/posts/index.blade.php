74d718ec0f97106a6ec49c7551bb58c23f2e4d9f
GitHubのユーザ名 <GitHub>
2022-11-04 11:41:41 +0900

    追加 tailwindcssの追加

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -3,6 +3,8 @@
     <head>
         <meta charset="utf-8">
         <title>blog</title>
+        <link href="{{ mix('css/app.css') }}" rel="stylesheet">
+        <link href="/css/index.css" rel="stylesheet" >
     </head>
     <body>
         <h1>blog</h1>
@@ -10,13 +12,13 @@
             <dl>
                 @foreach ($posts as $post)
                 <div>
-                    <dt>{{$post->title}}</dt>
+                    <dt class="underline decoration-sky-500">{{$post->title}}</dt>
                     <dd>{{$post->body}}</dd>
                 </div>
                 @endforeach
             </dl>
         </section>
-        <div class='paginate'>
+        <div class='paginate w-full'>
             {{ $posts->links() }}
         </div>
     </body>
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+	'./resources/views/posts/index.blade.php'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
