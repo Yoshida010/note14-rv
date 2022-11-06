a077c4e82724b1aa5aaffceb21a004f617902a21
GitHubのユーザ名 <GitHub>
2022-11-06 17:56:18 +0900

    追加 編集内容の保存処理の追加と保存時の入力チェック処理の追加

--- resources/views/posts/edit.blade.php
+++ resources/views/posts/edit.blade.php
@@ -10,17 +10,18 @@
         <h1 class="text-center">blog</h1>
         <section class="px-1">
             <dl>
-                <form action="/posts" method="POST">
+                <form action="/posts/{{$post->id}}" method="POST">
                     @csrf
+                    @method('PUT')
                     <div class="title">
                         <h2>Title</h2>
                         <input type="text" name="post[title]" placeholder="タイトル" value="{{$post->title}}" />
-                        <p class="title__error" style="color:red"></p>
+                        <p class="title__error" style="color:red">{{$errors->first('post.title')}}</p>
                     </div>
                     <div class="body">
                         <h2>Body</h2>
                         <textarea name="post[body]" placeholder="今日も1日お疲れさまでした。">{{$post->body}}</textarea>
-                        <p class="body__error" style="color:red"></p>
+                        <p class="body__error" style="color:red">{{$errors->first('post.body')}}</p>
                     </div>
                     <input type="submit" value="保存"/>
                 </form>
