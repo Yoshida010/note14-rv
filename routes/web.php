00640626c74e13ad0429a1c1eaa33126f9239214
GitHubのユーザ名 <GitHub>
2022-11-08 01:07:16 +0900

    追加 削除機能の追加

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,5 @@ Route::get('/posts/{post}/edit', [PostController::class, 'edit']);
 Route::get('/posts/{post}', [PostController::class, 'show']);
 
 Route::put('/posts/{post}' , [PostController::class, 'update']);
+
+Route::delete('/posts/{post}', [PostController::class, 'delete'] );
